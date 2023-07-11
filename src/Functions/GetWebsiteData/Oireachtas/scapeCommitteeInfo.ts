75e5839eda59bd3389f08b445dd029ad76158269
John Devlin <johnp.devlin2@gmail.com>
2023-07-11 18:55:00 +0100

    working scraper for individual committee page.

--- src/Functions/GetWebsiteData/Oireachtas/scapeCommitteeInfo.ts
+++ src/Functions/GetWebsiteData/Oireachtas/scapeCommitteeInfo.ts
@@ -1,145 +1,181 @@
 /** @format */
-
-import fetchMembers from '@/Functions/API-Calls/OireachtasAPI/members';
-import { Chamber, ChamberType, CommitteeType } from '@/Models/_utility';
+import { removeDuplicateObjects } from '@/Functions/Util/arrays';
+import { CommitteeType, URIpair } from '@/Models/_utility';
+import { Committee, PastCommitteeMember } from '@/Models/committee';
 import axios from 'axios';
 import * as cheerio from 'cheerio';
+import { Cheerio, CheerioAPI } from 'cheerio';
 
-type PastMember = {
-	name: string;
-	dateRange: {
-		date_start: Date;
-		date_end: Date;
-	};
-};
-
-export type Committee = {
-	name: string;
-	uri: string;
-	url: string;
-	house: Exclude<Chamber, 'dail & seanad'>;
-	types: CommitteeType[];
-	dail_no: number;
-	chair: string;
-	currentMembers: string[];
-	pastMembers?: PastMember[];
-} & Partial<ExpiredDetails>;
-
-type ExpiredDetails = {
-	historicText: string;
-	successorUrl?: string;
-	endDate: Date;
-};
-
+//Scrape committee information from the given URL.
 export default async function scrapeCommitteeInfo(
-	house_no: string | number,
+	house_no: number,
 	uri: string
 ): Promise<Committee | undefined> {
-	if (typeof house_no !== 'string') {
-		house_no = house_no.toString();
+	const url = `https://www.oireachtas.ie/en/committees/${house_no.toString()}/${uri}/`;
+	if (!url) throw new Error('No URL provided');
+
+	let response: string;
+	let $: cheerio.CheerioAPI;
+
+	// Fetch the committee page
+	try {
+		response = (await axios.get(`api/webscrape?url=${url}`)).data.text;
+		$ = cheerio.load(response);
+	} catch (err) {
+		return undefined;
 	}
 
-	const url = `https://www.oireachtas.ie/en/committees/${house_no}/${uri}/`;
-	if (!url) throw new Error('No url provided');
-
-	let response = (await axios.get(`api/webscrape?url=${url}`)).data.text;
-	let $ = cheerio.load(response);
-
-	// Extract the historic element
-	const historic: cheerio.Cheerio<cheerio.Element> | undefined = $(
+	// Extract the historic information
+	const historic: Cheerio<cheerio.Element> | undefined = $(
 		'.c-historic-committee-ribbon__message'
 	);
 	let historicText: string | undefined;
 	let successorUrl: string | undefined;
 	let endDate: Date | undefined;
 
-	// Check if historic element exists and extract relevant information
-	if (historic !== undefined) {
+	if (historic) {
 		historicText = historic.text().trim();
-		successorUrl = historic.attr('href');
+		successorUrl =
+			'https://www.oireachtas.ie' + historic.find('a').attr('href');
 		endDate = new Date($('.c-historic-committee-ribbon__date').text().trim());
 	}
 
+	// Fetch the membership page
 	try {
 		response = (await axios.get(`api/webscrape?url=${url}membership/`)).data
 			.text;
 	} catch (err) {
-		response = (await axios.get(`api/webscrape?url=${url}members/`)).data.text;
+		// If membership URL format is different, try the alternative
+		try {
+			response = (await axios.get(`api/webscrape?url=${url}members/`)).data
+				.text;
+		} catch (err) {
+			// Case for one special covid committee
+			return undefined;
+		}
 	}
+
 	$ = cheerio.load(response);
 
-	// Get the committee name
-	let committeeName = $('.c-hero__subtitle').text().trim();
-	let currentMembers: string[] = [];
-	let chair: string = '';
-	const house = (): Exclude<Chamber, 'dail & seanad'> => {
-		if (uri.includes('seanad')) return 'seanad';
-		else return 'dail';
-	};
-
-	// Extract the current members
-	$('.committee_member_link').each((index, element) => {
-		const memberName = $(element).text().trim();
-		currentMembers.push(memberName);
-	});
-
-	$('.member_box').each((i, el) => {
-		// Get chair
-		if ($(el).find('.committee_member_chair').length > 0) {
-			chair = $(el).find('.committee_member_link').text().trim();
-			currentMembers.push(chair);
-		}
-	});
-
+	// Extract committee information
+	const committeeName = $('.c-hero__subtitle').text().trim();
+	const chamber = uri.includes('seanad') ? 'seanad' : 'dail';
 	const committeeTypes = (): CommitteeType[] => {
 		const types: CommitteeType[] = [];
 		if ($('#joint').length > 0) types.push('joint');
 		if ($('#select').length > 0) types.push('select');
 		if ($('#standing').length > 0) types.push('standing');
-
 		return types;
 	};
 
-	const pastMembers: PastMember[] | undefined = [];
-	if (endDate !== undefined) {
-		// Extract the past members
-		$('.member_box_bottom-history.current-print').each((index, element) => {
-			const name = $(element).find('.committee_member_link').text();
+	const members = getMembers($);
+	const chair = getChair($) as URIpair;
+	const pastMembers = getPastMembers($);
 
-			// Format Dates
-			const dateText = $(element).find('p > i').text().trim();
-			const [startMonth, startYear, endMonth, endYear] = dateText
-				.split('-')
-				.map((str) => str.trim());
-
-			const date_start = new Date(`${startMonth} 01, ${startYear}`);
-			const date_end = new Date(`${endMonth} 01, ${endYear}`);
-
-			pastMembers.push({ name, dateRange: { date_start, date_end } });
-		});
-
-		// Remove past members from the array of current members
-		currentMembers = currentMembers.filter((member) =>
-			pastMembers.every((pastMember) => pastMember.name !== member)
-		);
-	}
+	// Remove past members from the array of current members
+	const filteredMembers = removePastMembers(members, pastMembers);
 
 	// Create the Committee object with the extracted information
 	const committee: Committee = {
 		name: committeeName,
-		uri: uri,
+		uri,
 		url,
 		types: committeeTypes(),
-		house: house(),
-		dail_no: parseInt(house_no),
+		chamber,
+		dail_no: house_no,
 		chair,
-		currentMembers,
-		pastMembers,
+		members: filteredMembers,
+		...(pastMembers && { pastMembers }),
 		...(historicText && { historicText }),
 		...(successorUrl && { successorUrl }),
 		...(endDate && { endDate }),
 	};
 
-	if (committee.house == 'seanad') console.log(committee);
 	return committee;
+}
+
+// Extract member URIs from the committee page.
+function getMembers($: CheerioAPI): URIpair[] {
+	const members: URIpair[] = [];
+	$('.committee_member_link').each((_index, element) => {
+		const memberName = $(element).text().trim();
+		const memberURI = $(element)
+			.attr('href')
+			?.split('r/')[1]
+			?.replace('/', '')
+			.trim() as string;
+		members.push({ name: memberName, uri: memberURI });
+	});
+
+	// Remove duplicates
+	const uniqueMembers = removeDuplicateObjects(members) as URIpair[];
+	return uniqueMembers;
+}
+
+// Extract the chair
+function getChair($: CheerioAPI): URIpair | undefined {
+	let chair: URIpair | undefined;
+	$('.member_box').each((_index, element) => {
+		if ($(element).find('.committee_member_chair').length > 0) {
+			const name = $(element).find('.committee_member_link').text().trim();
+			const uri = $(element)
+				.find('.committee_member_link')
+				.attr('href')
+				?.split('r/')[1]
+				?.replace('/', '') as string;
+			chair = { name, uri };
+		}
+	});
+	return chair;
+}
+
+// Extract past committee members from the committee page.
+function getPastMembers($: CheerioAPI): PastCommitteeMember[] | undefined {
+	const pastMembers: PastCommitteeMember[] = [];
+	$('.member_box_bottom-history.current-print').each((_index, element) => {
+		const name = $(element).find('.committee_member_link').text().trim();
+		if (name.length === 0) return;
+
+		const memberURI = $(element)
+			.find('.committee_member_link')
+			.attr('href')
+			?.split('r/')[1]
+			?.replace('/', '')
+			.trim() as string;
+
+		// Format Dates
+		const dateText = $(element).find('p > i').text().trim();
+		const [startMonth, startYear, endMonth, endYear] = dateText
+			.split('-')
+			.map((str) => str.trim());
+		const date_start = new Date(`${startYear}-${startMonth}-01`);
+		const date_end = new Date(`${endYear}-${endMonth}-01`);
+
+		if (!date_start.getTime() || !date_end.getTime()) {
+			console.log(dateText);
+		}
+
+		pastMembers.push({
+			name,
+			uri: memberURI,
+			dateRange: { date_start, date_end },
+		});
+	});
+
+	// Remove duplicates
+	const uniquePastMembers = removeDuplicateObjects(
+		pastMembers
+	) as PastCommitteeMember[];
+	return uniquePastMembers.length > 0 ? uniquePastMembers : undefined;
+}
+
+// Remove past members from the array of current members.
+function removePastMembers(
+	members: URIpair[],
+	pastMembers: PastCommitteeMember[] | undefined
+): URIpair[] {
+	if (!pastMembers) return members;
+	return members.filter((member) =>
+		pastMembers.every((pastMember) => pastMember.uri !== member.uri)
+	);
 }
