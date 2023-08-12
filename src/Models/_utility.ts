9acec455ae4795a7839243bdcf71bdaac8bbcfec
John Devlin <johnp.devlin2@gmail.com>
2023-08-12 20:58:51 +0100

    f()s and types re: dates and refactoring / reorg

--- src/Models/_utility.ts
+++ src/Models/_utility.ts
@@ -7,14 +7,14 @@ export type BinaryChamber = 'dail' | 'seanad';
 export type Chamber = BinaryChamber | 'dail & seanad';
 export type CommitteeType = 'select' | 'joint' | 'standing' | 'working group';
 export type RepresentType = 'constituency' | 'panel';
+
 export type Outcome = 'carried' | 'lost';
 export type QuestionType = 'oral' | 'written';
-export type DateRange = {
-	date_start: Date | string;
-	date_end: Date | string;
-};
+
 export type MemberBaseKeys = { houseCode: BinaryChamber } & URIpair;
 export type URIpair = {
 	name: string;
 	uri: string;
 };
+
+export type GroupType = 'dail' | 'seanad' | 'party' | 'constituency' | 'member';
