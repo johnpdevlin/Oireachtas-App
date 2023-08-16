3a2983356f25c0ac0945b5d46403b9ba6970058c
John Devlin <johnp.devlin2@gmail.com>
2023-08-16 21:34:56 +0100

    f() to parse offices and create types

--- src/Functions/Processes/Member/Oireachtas/API/Parse/Memberships/offices.ts
+++ src/Functions/Processes/Member/Oireachtas/API/Parse/Memberships/offices.ts
@@ -1,37 +1,40 @@
 /** @format */
 
-import { RawMemberOffice, RawMembership } from '@/Models/OireachtasAPI/member';
-import { BinaryChamber, ChamberType } from '@/Models/_utility';
+import { getEndDateObj } from '@/Functions/Util/dates';
+import { BinaryChamber } from '@/Models/_utility';
+import { DateRangeStr, OirDate } from '@/Models/dates';
+import { RawOffice } from '.';
+import { getEndDateStr } from '../../../../../../Util/dates';
+import { MemberOffice, OfficeType } from '@/Models/DB/office';
 
-type RawOffice = {
-	name: string;
-	type: string;
-	dateRange: { start: string; end: string | null | undefined };
-	house: {
-		dateRange: { start: string; end: string | null };
-		houseCode: BinaryChamber;
-		uri: string;
-		houseNo: string;
-		showAs: string;
-		chamberType: ChamberType;
-	};
-};
-export function parseOffices(membership: RawMembership): RawOffice[] {
-	const offices: RawOffice[] = [];
-	const house = { ...membership.house, dateRange: membership.dateRange };
-	membership.offices.map((off: { office: RawMemberOffice }) => {
-		const office = off.office;
-		offices.push({
-			name: office.officeName.showAs,
-			type: parseOfficeType(office.officeName.showAs),
-			dateRange: office.dateRange,
-			house,
-		});
+export default function parseAndFormatOffices(
+	offices: RawOffice[]
+): MemberOffice[] {
+	const parsed: MemberOffice[] = [];
+	offices.forEach((off) => {
+		const office = {
+			name: off.officeName.showAs,
+			type: parseOfficeType(off.officeName.showAs),
+			chamber: off.house.houseCode as BinaryChamber,
+			houseNo: parseInt(off.house.houseNo),
+			chamberStr: off.house.showAs,
+			dateRange: {
+				start: new Date(off.dateRange.start),
+				end: getEndDateObj(off.dateRange.end as OirDate | undefined | null),
+			},
+			dateRangeStr: {
+				start: off.dateRange.start,
+				end: getEndDateStr(off.dateRange.end as OirDate | undefined | null),
+			} as DateRangeStr,
+		};
+		parsed.push(office);
 	});
-	return offices;
+	return parsed
+		.filter(Boolean)
+		.sort((a, b) => a.dateRange.start.getTime() - b.dateRange.start.getTime());
 }
 
-function parseOfficeType(office: string): string {
+function parseOfficeType(office: string): OfficeType {
 	if (office.toLowerCase().includes('of state')) return 'junior';
 	else return 'senior';
 }
