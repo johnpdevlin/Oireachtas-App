3ee44ad73e2b1cfe7ad8161230b03d809f81d4f9
John Devlin <johnp.devlin2@gmail.com>
2023-08-17 19:18:24 +0100

    add logs and filter out undefined values

--- src/Functions/Processes/Committee/allCommitteesInfo.ts
+++ src/Functions/Processes/Committee/allCommitteesInfo.ts
@@ -8,22 +8,27 @@ import scrapeCommitteesBaseDetails, {
 } from '@/Functions/GetWebsiteData/Oireachtas/Committee/WebPage/baseDetails';
 
 export async function processAllCommitteeInfo(): Promise<Committee[]> {
+	console.log('Scraping data for all committees has begun.');
 	// Get all committee base details
 	const allCommitteesBaseDetails = await scrapeCommitteesBaseDetails();
 
 	// Get details for members of committee and other details
-	const committees = await allCommitteesBaseDetails.reduce(
-		async (resultsPromise: Promise<Committee[]>, c: BaseCommittee) => {
-			const results = await resultsPromise;
-			const info = await scrapeCommitteePageInfo(c.dailNo, c.uri);
-			if (info?.name) {
-				results.push(info);
-			} else {
-				console.error('Issue with committee scraping:', c.uri);
-			}
-			return results;
-		},
-		Promise.resolve([])
-	);
+	const committees = await (
+		await allCommitteesBaseDetails.reduce(
+			async (resultsPromise: Promise<Committee[]>, c: BaseCommittee) => {
+				const results = await resultsPromise;
+				const info = await scrapeCommitteePageInfo(c.dailNo, c.uri);
+				if (info?.name) {
+					results.push(info);
+				} else {
+					console.error('Issue with committee scraping:', c.uri);
+				}
+				return results;
+			},
+			Promise.resolve([])
+		)
+	).filter(Boolean);
+
+	console.log('Committee scraping process complete.');
 	return committees;
 }
