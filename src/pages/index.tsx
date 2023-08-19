007a7954194ed3c3f0296b619f26cb0e3c36f584
John Devlin <johnp.devlin2@gmail.com>
2023-08-19 22:58:17 +0100

    minor changes to make branch up to date.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,9 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
-import prcAttendanceReports from '@/Functions/Processes/Attendance/prcAttendanceReports';
-import { prcParticipation } from '@/Functions/Processes/Participation/prcParticipation';
-import processAllMemberDetails from '@/Functions/Processes/Member/allMemberDetails';
+// import prcAttendanceReports from '@/Functions/Processes/Attendance/prcAttendanceReports';
+// import { prcParticipation } from '@/Functions/Processes/Participation/prcParticipation';
+import processAllMemberDetails from '@/Functions/Processes/Member/Get/allMemberDetails';
 
 const inter = Inter({ subsets: ['latin'] });
 
