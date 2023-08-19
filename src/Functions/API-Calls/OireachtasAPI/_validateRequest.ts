5f99f79d4fcc303c6d6fef3a0fc2f489010b8792
John Devlin <johnp.devlin2@gmail.com>
2023-08-19 01:35:18 +0100

    minor logic change and more explicit types

--- src/Functions/API-Calls/OireachtasAPI/_validateRequest.ts
+++ src/Functions/API-Calls/OireachtasAPI/_validateRequest.ts
@@ -1,16 +1,15 @@
 /** @format */
 
 import { dateToYMDstring } from '@/Functions/Util/dates';
-import isValidDate from '@/Functions/Validate/validateOireachtasDate';
-
-import { Chamber, QuestionType } from '@/Models/_utility';
+import { Chamber, QuestionType, MemberURI } from '@/Models/_utility';
+import { OirDate } from '@/Models/dates';
 import { th } from 'date-fns/locale';
 
 export type OireachtasRequest = {
 	uri?: string;
-	member_id?: string;
-	date_start?: string | Date;
-	date_end?: string | Date;
+	member_id?: MemberURI;
+	date_start?: OirDate | Date;
+	date_end?: OirDate | Date;
 	house_no?: number;
 	chamber_id?: Chamber;
 	const_code?: string;
@@ -30,15 +29,8 @@ export default function validateOireachtasRequest(
 		if (props.date_end instanceof Date) {
 			props.date_end = dateToYMDstring(props.date_end);
 		}
-		if (props.date_start! || props.date_end!) {
-			if (props.date_end == undefined) {
-				props.date_end = dateToYMDstring(new Date());
-			}
-			if (isValidDate([props.date_start!, props.date_end!]) === false) {
-				throw new Error(
-					`${props.date_start!} & ${props.date_end!} : Invalid date(s), should be in format YYYY-MM-DD`
-				);
-			}
+		if (props.date_end == undefined) {
+			props.date_end = dateToYMDstring(new Date());
 		}
 	} catch (e) {
 		console.error(e);
