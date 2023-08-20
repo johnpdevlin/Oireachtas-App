19531e57de855c1feb4b8179f3b3a5f48a6b5456
John Devlin <johnp.devlin2@gmail.com>
2023-08-20 22:10:41 +0100

    file to initialise firestore

---
+++ src/FirestoreDB/index.ts
@@ -0,0 +1,17 @@
+/** @format */
+
+import admin, { ServiceAccount } from 'firebase-admin';
+import serviceAccount from './service-account-key.json';
+
+if (!admin.apps.length) {
+	try {
+		admin.initializeApp({
+			credential: admin.credential.cert(serviceAccount as ServiceAccount),
+		});
+	} catch (error) {
+		console.log('Firebase admin initialization error', error.stack);
+	}
+}
+
+const firestore = admin.firestore();
+export default firestore;
