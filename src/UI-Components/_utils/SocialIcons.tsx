cc8741ac172e9dd63718492745db7e2e50f50903
John Devlin <johnp.devlin2@gmail.com>
2023-08-29 21:14:52 +0100

    basic component for social icons

---
+++ src/UI-Components/_utils/SocialIcons.tsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import { Stack } from '@mui/material';
+
+function ProfileIcons(props: { type: string; link: string }) {
+	return (
+		<>
+			<Stack direction='row' sx={{ mt: 2 }}></Stack>
+		</>
+	);
+}
+
+export default ProfileIcons;
