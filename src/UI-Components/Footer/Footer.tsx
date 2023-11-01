96cb36dca1af13bb1bcacd2e315060af8907a960
John Devlin <johnp.devlin2@gmail.com>
2023-11-01 14:07:22 +0000

    create footer component

---
+++ src/UI-Components/Footer/Footer.tsx
@@ -0,0 +1,53 @@
+/** @format */
+
+import webdetails from '@/Data/website';
+import React from 'react';
+import { Box, Grid, Link, Stack, Typography } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
+export default function Footer() {
+	return (
+		<footer>
+			<Box
+				component='footer'
+				bgcolor='powderblue'
+				color='white'
+				mt={10}
+				px={10}
+				py={3}>
+				<Grid container justifyContent='space-between' alignItems='center'>
+					<Grid item display='flex' justifyContent='flex-start'>
+						<Typography
+							variant='h6'
+							sx={{ textShadow: '0px 0px 1px rgba(0, 0, 0, 6)' }}>
+							&copy; {webdetails.currentYear} {webdetails.owner}
+						</Typography>
+					</Grid>
+					<Grid item alignItems={'flex-end'}>
+						<Stack direction='row' gap={1} justifyContent='flex-end'>
+							{webdetails.links.github && (
+								<Link
+									href={webdetails.links.github}
+									target='_blank'
+									rel='noopener'
+									color='inherit'>
+									<GitHubIcon fontSize='large' />
+								</Link>
+							)}
+							{webdetails.links.twitter && (
+								<Link
+									href={webdetails.links.twitter}
+									target='_blank'
+									rel='noopener'
+									color='inherit'>
+									<TwitterIcon fontSize='large' />
+								</Link>
+							)}
+						</Stack>
+					</Grid>
+				</Grid>
+			</Box>
+		</footer>
+	);
+}
