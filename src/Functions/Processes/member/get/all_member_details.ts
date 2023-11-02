b0da48100d69dfa7197d39c61d001816e35e6368
John Devlin <johnp.devlin2@gmail.com>
2023-11-02 11:24:54 +0000

    Add comments

--- src/Functions/Processes/member/get/all_member_details.ts
+++ src/Functions/Processes/member/get/all_member_details.ts
@@ -18,15 +18,25 @@ import checkGender from '@/functions/APIs_/Irish_Names_';
 export default async function getAllMemberDetails() {
 	console.info('Process to get all member details begun...');
 
+	// Data directly from Oireachtas API
 	const apiData = (await getAllMembersAPIdetails(undefined, {
 		house_no: 33,
 	})) as MemberAPIdetails[];
+
+	// Gets all URIs
 	const uris = apiData!.map((member: { uri: MemberURI }) => member.uri);
+
+	// Gets Data from Oireachtas Website profiles
 	const oirData = await getAllMembersOirData(uris!);
+
+	// Gets data from Wikipedia
 	const wikiDetails = await getTDsWikiData(33);
+
+	// Function to merge / bind all data together to create objects
 	const mergedData = await bindAllData(uris, oirData, wikiDetails, apiData);
 
 	console.info('Process to get all member details completed.');
+
 	return mergedData;
 }
 
@@ -40,14 +50,20 @@ async function bindAllData(
 	const girlNames = (await fetchNames('girl')) as Record<number, string>;
 
 	const bound = await Promise.all(
+		// Iterate over each URI
 		uris.map(async (uri: MemberURI) => {
+			// Find TD API and OIR data by URI
 			const api = apiData.find((data) => data.uri === uri);
 			const oir = oirData.find((data: OirData) => data.uri === uri);
+
+			// Find Wiki Data by name
 			let wiki = wikiData.find(
 				(data: WikiProfileDetails) =>
 					data.wikiURI.toLowerCase().includes(api!.lastName!.toLowerCase()) &&
 					data.wikiURI.toLowerCase().includes(api!.firstName!.toLowerCase())
 			);
+
+			// If no initial wiki data match by name
 			if (!wiki) {
 				const fullName = api!.fullName.toLowerCase();
 				const matches = similarity.findBestMatch(
@@ -57,6 +73,7 @@ async function bindAllData(
 				wiki = wikiData.find((w) => w.wikiURI === matches.bestMatch.target);
 			}
 
+			// Get gender by name match
 			const gender = await checkGender(
 				api!.firstName.toLowerCase(),
 				boyNames,
