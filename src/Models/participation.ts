8959d529e2ba980d18ce859f80a87e536d000479
John Devlin <johnp.devlin2@gmail.com>
2023-11-20 15:25:21 +0000

    rework participation models (further models may be used)

--- src/Models/participation.ts
+++ src/Models/participation.ts
@@ -1,15 +1,43 @@
 /** @format */
 
-import { Chamber } from './_utils';
+import { ChamberType, CommitteeType } from './_utils';
 
-export type MembershipType =
-	| 'house'
-	| 'party'
-	| 'constituency'
-	| 'panel'
-	| 'committee';
+/** 
+	INDIVIDUAL TD / SENATORS
+**/
 
-export type groupType =
+type ParticipationRecord = {
+	uri: string;
+	type: ChamberType;
+	recordedPresent: boolean;
+	contributed: boolean;
+	speeches: number;
+	votes: number;
+	votesMissed?: number;
+};
+
+type DayParticipationRecord = {
+	date: Date;
+	dayOfWeek: 0 | 1 | 2 | 3 | 4 | 5 | 6;
+} & ParticipationRecord;
+
+export type DayParticipationHouseRecord = {
+	type: 'house';
+	oralQuestions: number;
+	writtenQuestions: number;
+} & DayParticipationRecord;
+
+export type DayParticipationCommitteeRecord = {
+	type: 'committee';
+	committeeUri: string;
+	committeeType: CommitteeType;
+} & DayParticipationRecord;
+
+/**
+ * Group Participation Records
+ */
+
+type groupType =
 	| 'td'
 	| 'house'
 	| 'party'
@@ -17,61 +45,19 @@ export type groupType =
 	| 'panel'
 	| 'committee';
 
-export type DaySpeeches = {
-	houseSpeeches?: number;
-	committeeSpeeches?: number;
-};
-
-export type DayQuestions = {
-	oralQuestions?: number;
-	writtenQuestions?: number;
-};
-
-export type DayVotes = {
-	houseVotes?: number;
-	committeeVotes?: number;
-	houseVotesMissed?: number;
-};
-
-export type DayCommitteeVotes = {
-	committeeUri: string;
-	date: Date;
-	committeeVotes: number;
-	committeeVotesMissed: number;
-};
-
-export interface ParticipationRecord {
-	type?: groupType;
-	uri: string;
-	house: Chamber;
-	houseNo?: number;
-	members: Array<string>;
-	houseContributed: number;
-	noHouseContribution: number;
-	committeePresent?: number;
-	committeeAbsent?: number;
-	houseSpeeches: number;
-	committeeSpeeches?: number;
-	oralQuestions?: number;
-	writtenQuestions?: number;
-	houseVotes: number;
-	committeeVotes?: number;
-}
-
-export type DayParticipationRecord = {
-	name: string;
-	uri: string;
-	date: Date;
-	chamber: Chamber;
-	houseNo: number;
-	housePresent?: boolean;
-	committeePresent?: boolean;
-} & DaySpeeches &
-	DayQuestions &
-	DayVotes;
-
 export type GroupParticipationRecord = {
 	type?: groupType;
-	pastMembers?: Array<{ uri: string; days: number }>;
-	count?: number | undefined;
-} & ParticipationRecord;
+	uri: string;
+	present: number;
+	notPresent: number;
+	contributed: number;
+	notContributed: number;
+	contributions: number;
+	votes: number;
+	questions?: number;
+};
+
+export type GroupParticipationDayRecord = {
+	date: Date;
+	dayOfWeek: 0 | 1 | 2 | 3 | 4 | 5 | 6;
+} & GroupParticipationRecord;
