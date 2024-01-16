065d0f639d1d564406efcc25acb1ff24986f04e6
John Devlin <johnp.devlin2@gmail.com>
2024-01-16 17:28:53 +0000

    file for document url reference points

---
+++ src/Data/member-interests/register-urls.ts
@@ -0,0 +1,118 @@
+/** @format */
+
+const urls = [
+	{
+		year: 2022,
+		url: 'https://data.oireachtas.ie/ie/oireachtas/members/registerOfMembersInterests/dail/2023/2023-02-22_register-of-member-s-interests-dail-eireann-2022_en.pdf',
+	},
+	{
+		year: 2021,
+		url: 'https://data.oireachtas.ie/ie/oireachtas/members/registerOfMembersInterests/dail/2022/2022-02-16_register-of-members-interests-dail-eireann_en.pdf',
+	},
+	{
+		year: 2020,
+		url: 'https://data.oireachtas.ie/ie/oireachtas/members/registerOfMembersInterests/dail/2021/2021-02-25_register-of-members-interests-dail-eireann_en.pdf',
+	},
+	{
+		year: 2019,
+		url: 'https://data.oireachtas.ie/ie/oireachtas/members/registerOfMembersInterests/dail/2020/2020-03-03_register-of-members-interests-dail-eireann_en.pdf',
+	},
+	{
+		year: 2018,
+		url: 'https://data.oireachtas.ie/ie/oireachtas/members/registerOfMembersInterests/dail/2019/2019-02-13_register-of-members-interests-dail-eireann_en.pdf',
+	},
+	{
+		year: 2017,
+		url: 'https://data.oireachtas.ie/ie/oireachtas/members/registerOfMembersInterests/dail/2018/2018-02-14_register-of-members-interests-dail-eireann_en.pdf',
+	},
+	{
+		year: 2016,
+		url: 'https://data.oireachtas.ie/ie/oireachtas/members/registerOfMembersInterests/dail/2017/2017-03-10_register-of-members-interests-dail-eireann_en.pdf',
+	},
+	{
+		year: 2015,
+		url: 'https://data.oireachtas.ie/ie/oireachtas/members/registerOfMembersInterests/dail/2016/2016-03-01_register-of-members-interests-dail-eireann_en.pdf',
+	},
+	{
+		year: 2014,
+		url: 'https://data.oireachtas.ie/ie/oireachtas/members/registerOfMembersInterests/dail/2015/2015-03-11_register-of-members-interests-dail-eireann_en.pdf',
+	},
+	{
+		year: 2013,
+		url: 'https://data.oireachtas.ie/ie/oireachtas/members/registerOfMembersInterests/dail/2014/2014-03-25_register-of-members-interests-dail-eireann_en.pdf',
+	},
+	{
+		year: 2012,
+		url: 'https://data.oireachtas.ie/ie/oireachtas/members/registerOfMembersInterests/dail/2013/2013-02-28_register-of-members-interests-dail-eireann_en.pdf',
+	},
+	{
+		year: 2011,
+		url: 'https://data.oireachtas.ie/ie/oireachtas/members/registerOfMembersInterests/dail/2012/2012-03-30_register-of-members-interests-dail-eireann_en.pdf',
+	},
+	{
+		year: 2010,
+		url: 'https://data.oireachtas.ie/ie/oireachtas/members/registerOfMembersInterests/dail/2011/2011-03-31_register-of-members-interests-dail-eireann_en.pdf',
+	},
+	{
+		year: 2009,
+		url: 'https://data.oireachtas.ie/ie/oireachtas/members/registerOfMembersInterests/dail/2010/2010-03-12_register-of-members-interests-dail-eireann_en.pdf',
+	},
+	{
+		year: 2008,
+		url: 'https://data.oireachtas.ie/ie/oireachtas/members/registerOfMembersInterests/dail/2009/2009-03-12_register-of-members-interests-dail-eireann_en.pdf',
+	},
+	{
+		year: 2007,
+		url: 'https://data.oireachtas.ie/ie/oireachtas/members/registerOfMembersInterests/dail/2008/2008-03-11_register-of-members-interests-dail-eireann_en.pdf',
+	},
+	{
+		year: 2006,
+		url: 'https://data.oireachtas.ie/ie/oireachtas/members/registerOfMembersInterests/dail/2007/2007-03-30_register-of-members-interests-dail-eireann_en.pdf',
+	},
+	{
+		year: 2005,
+		url: 'https://data.oireachtas.ie/ie/oireachtas/members/registerOfMembersInterests/dail/2006/2006-03-31_register-of-members-interests-dail-eireann_en.pdf',
+	},
+	{
+		year: 2004,
+		url: 'https://data.oireachtas.ie/ie/oireachtas/members/registerOfMembersInterests/dail/2005/2005-05-04_register-of-members-interests-dail-eireann_en.pdf',
+	},
+	{
+		year: 2003,
+		url: 'https://data.oireachtas.ie/ie/oireachtas/members/registerOfMembersInterests/dail/2004/2004-05-21_register-of-members-interests-dail-eireann_en.pdf',
+	},
+	{
+		year: 2002,
+		url: 'https://data.oireachtas.ie/ie/oireachtas/members/registerOfMembersInterests/dail/2003/2003-05-09_register-of-members-interests-dail-eireann_en.pdf',
+	},
+	{
+		year: 2001,
+		url: 'https://data.oireachtas.ie/ie/oireachtas/members/registerOfMembersInterests/dail/2002/2002-07-10_register-of-members-interests-dail-eireann_en.pdf',
+	},
+	{
+		year: 2000,
+		url: 'https://data.oireachtas.ie/ie/oireachtas/members/registerOfMembersInterests/dail/2001/2001-05-11_register-of-members-interests-dail-eireann_en.pdf',
+	},
+	{
+		year: 1999,
+		url: 'https://data.oireachtas.ie/ie/oireachtas/members/registerOfMembersInterests/dail/2000/2000-06-26_register-of-members-interests-dail-eireann_en.pdf',
+	},
+	{
+		year: 1998,
+		url: 'https://data.oireachtas.ie/ie/oireachtas/members/registerOfMembersInterests/dail/1999/1999-05-12_register-of-members-interests-dail-eireann_en.pdf',
+	},
+	{
+		year: 1997,
+		url: 'https://data.oireachtas.ie/ie/oireachtas/members/registerOfMembersInterests/dail/1998/1998-04-22_register-of-members-interests-dail-eireann_en.pdf',
+	},
+	{
+		year: 1996,
+		url: 'https://data.oireachtas.ie/ie/oireachtas/members/registerOfMembersInterests/dail/1997/1997-04-18_register-of-members-interests-dail-eireann_en.pdf',
+	},
+	{
+		year: 1995,
+		url: 'https://data.oireachtas.ie/ie/oireachtas/members/registerOfMembersInterests/dail/1996/1996-04-04_register-of-members-interests-dail-eireann_en.pdf',
+	},
+];
+
+export default urls;
