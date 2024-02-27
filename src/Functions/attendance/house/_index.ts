c6dd68d2249c25519fdfa8b3936148fd54bf912e
John Devlin <johnp.devlin2@gmail.com>
2024-02-27 12:24:18 +0000

    f()s to convert sitting days records to attendance type and aggregate

---
+++ src/Functions/attendance/house/_index.ts
@@ -0,0 +1,43 @@
+/** @format */
+
+import { BinaryChamber } from '@/models/_utils';
+import { processSittingReportsByTerm } from './report/_house_attendance';
+import { getPossibleSittingDates } from './_utils/get_possible_sitting_dates';
+import { getMemberHouseAttendanceRecord } from './member_records/_attendance_record';
+import { aggregateMembershipAttendanceRecords } from '../_utils/aggregate_records/_constits+parties';
+import fetchMembers from '@/functions/APIs/Oireachtas/member/raw/_member_details';
+import { aggregateAllMembersAttendanceRecords } from '../_utils/aggregate_records/_all_members';
+
+async function processHouseAttendanceByTerm(
+	chamber: BinaryChamber,
+	house_no: number
+) {
+	const members = await fetchMembers({ chamber, house_no });
+
+	const sittingReports = await processSittingReportsByTerm(chamber, house_no);
+	const possibleSittingDates = getPossibleSittingDates(sittingReports);
+	const ind_member_records = sittingReports.map((sr) =>
+		getMemberHouseAttendanceRecord(sr, possibleSittingDates)
+	);
+	const membership_records = await aggregateMembershipAttendanceRecords(
+		ind_member_records,
+		members!
+	);
+	const all_members = aggregateAllMembersAttendanceRecords(
+		house_no.toString(),
+		ind_member_records,
+		members!
+	);
+
+	return {
+		possibleSittingDates,
+		records: {
+			ind_member_records,
+			all_members,
+			membership_records,
+			...all_members,
+		},
+	};
+}
+
+export { processHouseAttendanceByTerm };
---
+++ src/Functions/attendance/house/member_records/_attendance_record.ts
@@ -0,0 +1,49 @@
+/** @format */
+import { aggregateDatesToMonthsArray } from '@/functions/_utils/dates';
+import {
+	AttendanceRecord,
+	SittingDaysRecord,
+} from '../../../../models/attendance';
+import { initializeAttendanceSummary } from '../../_utils/init_attendance_summary';
+function getMemberHouseAttendanceRecord(
+	record: SittingDaysRecord,
+	possibleSittings: Date[]
+): AttendanceRecord {
+	const attendanceRecord = initializeAttendanceSummary(
+		record.uri!,
+		record.year!,
+		'member'
+	) as AttendanceRecord;
+
+	attendanceRecord.present = aggregateDatesToMonthsArray(record.sittingDates);
+	attendanceRecord.also_present = aggregateDatesToMonthsArray(
+		record.otherDates
+	);
+
+	attendanceRecord.dateRange = record.dateRange;
+	const { start, end } = record.dateRange;
+	// Gets sitting dates relevant for record period
+	const relevantSittings = possibleSittings.filter(
+		(s) => s >= start && s <= end!
+	);
+	attendanceRecord.absent = getAbsent(
+		relevantSittings,
+		attendanceRecord.present.flat()
+	);
+
+	return attendanceRecord;
+}
+
+function getAbsent(relevant: Date[], present: Date[]): Date[][] {
+	const absentMap = new Map<string, Date>();
+
+	relevant.forEach((rel) => {
+		if (!present.find((pres) => pres.getTime() === rel.getTime()))
+			absentMap.set(rel.toString(), rel);
+	});
+
+	const absent = aggregateDatesToMonthsArray(Array.from(absentMap.values()));
+	return absent;
+}
+
+export { getMemberHouseAttendanceRecord };
