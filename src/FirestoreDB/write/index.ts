1efbe795c62cce98b3021968e06ed2d86154771e
John Devlin <johnp.devlin2@gmail.com>
2024-03-28 20:15:22 +0000

    simplify write to DB f()

--- src/FirestoreDB/write/index.ts
+++ src/FirestoreDB/write/index.ts
@@ -6,14 +6,6 @@ type FirestoreData = {
 	[key: string]: any; // Define the structure of your data
 };
 
-type FirestoreRequest = {
-	collection: string;
-	data: FirestoreData;
-	id?: string;
-	id_field?: string;
-	overwrite?: boolean;
-};
-
 export const writeObjToFirestore = async (
 	collection: string,
 	data: FirestoreData,
@@ -28,18 +20,12 @@ export const writeObjToFirestore = async (
 			data
 		)}`;
 
-		if (id) {
-			url += `&id=${id}`;
-		}
-		if (id_field) {
-			url += `&id_field=${id_field}`;
-		}
-		if (overwrite) {
-			url += `&overwrite=${overwrite}`;
-		}
+		if (id) url += `&id=${id}`;
+		if (id_field) url += `&id_field=${id_field}`;
+		if (overwrite) url += `&overwrite=${overwrite}`;
 
 		const response = await axios.post(url);
-		console.log(response.data);
+
 		return response.data;
 	} catch (error) {
 		throw error;
