578f94ecaae511ae8a5d6991de26c134945a6355
John Devlin <johnp.devlin2@gmail.com>
2024-03-29 18:35:27 +0000

    change error handling, add retry logic

--- src/FirestoreDB/write/index.ts
+++ src/FirestoreDB/write/index.ts
@@ -13,21 +13,32 @@ export const writeObjToFirestore = async (
 	id_field?: string,
 	overwrite?: boolean
 ): Promise<any> => {
-	try {
-		const token = process.env.API_SECRET;
+	let retries = 5;
 
-		let url = `/api/firestore?&token=${token}&collection=${collection}&data=${JSON.stringify(
-			data
-		)}`;
+	while (retries > 0) {
+		try {
+			const token = process.env.API_SECRET;
 
-		if (id) url += `&id=${id}`;
-		if (id_field) url += `&id_field=${id_field}`;
-		if (overwrite) url += `&overwrite=${overwrite}`;
+			let url = `/api/firestore?&token=${token}&collection=${collection}&data=${JSON.stringify(
+				data
+			)}`;
 
-		const response = await axios.post(url);
+			if (id) url += `&id=${id}`;
+			if (id_field) url += `&id_field=${id_field}`;
+			if (overwrite) url += `&overwrite=${overwrite}`;
 
-		return response.data;
-	} catch (error) {
-		throw error;
+			const response = await axios.post(url);
+
+			return response.data;
+		} catch (error) {
+			if (retries === 1) {
+				// If this was the last retry, throw the error
+				throw error;
+			} else {
+				// Retry logic
+				console.error(`Request failed, ${retries} retries left. Retrying...`);
+				retries--;
+			}
+		}
 	}
 };
