279a4608a72ad589ae51dbca39edfd2635764fa6
John Devlin <johnp.devlin2@gmail.com>
2024-04-03 19:26:37 +0100

    simplify fetching f(), add additional filtering remove unnecessary code

--- src/FirestoreDB/read/page_data/td.ts
+++ src/FirestoreDB/read/page_data/td.ts
@@ -1,12 +1,11 @@
 /** @format */
 
-import { MemberBioData } from '@/functions/processes/td/_agg_td_details_by_house';
 import getDocFromDB from '../single/[uri]';
 import getDocsBatchFromDB from '../multiple/batch';
 import { GroupType } from '@/models/_utils';
-import { MemberCommittee } from '@/models/oireachtasApi/Formatted/Member/committee';
-import { MemberPageData } from '@/models/ui/member';
+import { MemberBioData, MemberPageData } from '@/models/ui/member';
 import { AttendanceData } from '@/models/ui/attendance';
+import { AttendanceRecord } from '@/models/attendance';
 
 export default async function fetchTDpageData(
 	uri: string
@@ -16,11 +15,7 @@ export default async function fetchTDpageData(
 	const party = bioData.parties[0].uri;
 	const constituency = bioData.constituencies!.dail![0].uri;
 
-	const attendanceData = await getAttendanceData(
-		party,
-		constituency,
-		bioData.committees
-	);
+	const attendanceData = await getAttendanceData(uri, party, constituency);
 
 	return {
 		bio: bioData,
@@ -29,10 +24,11 @@ export default async function fetchTDpageData(
 }
 
 const getAttendanceData = async (
+	memberID: string,
 	partyID: string,
-	constitID: string,
-	committees: { current: MemberCommittee[]; past: MemberCommittee[] }
+	constitID: string
 ): Promise<AttendanceData> => {
+	const member = { group_type: 'member', id: memberID };
 	const dail = { group_type: 'dail', id: 'dail' };
 	const party = {
 		group_type: 'party',
@@ -42,21 +38,32 @@ const getAttendanceData = async (
 		group_type: 'constituency',
 		id: constitID,
 	};
-	const comms = [...committees.current, ...committees.past].map((com) => {
-		return { group_type: 'committee', id: com.uri };
-	}) as { group_type: GroupType; id: string }[];
 
-	const params = [party, constituency, dail] as {
+	const params = [member, party, constituency, dail] as {
 		group_type: GroupType;
 		id: string;
 	}[];
 	const houseAttendance = await getDocsBatchFromDB('house-attendance', params);
-	const committeeAttendance = await getDocsBatchFromDB('committee-attendance', [
-		...params,
-		// ...comms,
-	]);
+	const committeeAttendance = await fetchCommitteeAttendance(params);
+
 	return {
 		house: houseAttendance,
 		committee: committeeAttendance,
 	} as AttendanceData;
 };
+
+async function fetchCommitteeAttendance(
+	params: { group_type: GroupType; id: string }[]
+): Promise<Record<GroupType, AttendanceRecord[]>> {
+	const committeeAttendance = (await getDocsBatchFromDB(
+		'committee-attendance',
+		params
+	)) as Record<GroupType, AttendanceRecord[]>;
+
+	// Filter out committee records related to members
+	committeeAttendance.member = committeeAttendance.member.filter((com) =>
+		com.record_uri.startsWith('member')
+	);
+
+	return committeeAttendance;
+}
