a8673b736fda989f441e8a37d18e37f1dbdafbbb
John Devlin <johnp.devlin2@gmail.com>
2024-04-04 21:00:06 +0100

    refactor grouped select component

--- src/UI-Components/_utils/Select/GroupedSelect.tsx
+++ src/UI-Components/_utils/Select/GroupedSelect.tsx
@@ -1,49 +1,63 @@
 /** @format */
 
-import * as React from 'react';
 import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import ListSubheader from '@mui/material/ListSubheader';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import { Dispatch, SetStateAction } from 'react';
 
-export default function GroupedSelect(props: {
+type SelectProps = {
 	label: string;
 	groupings: {
 		category: string;
 		groups: { label: string; value: string | number }[];
 	}[];
-}) {
+	value: number | string;
+	setValue: Dispatch<SetStateAction<string>>;
+	overall?: string;
+};
+
+export default function GroupedSelect({
+	label,
+	groupings,
+	value,
+	setValue,
+	overall,
+}: SelectProps) {
 	const optionGroups = () => {
-		return props.groupings.map((groupings) => {
-			return (
-				<>
-					<optgroup label={groupings.category}>
-						{groupings.groups.map((group) => {
-							return (
-								<>
-									<option value={group.value}>{group.label}</option>
-								</>
-							);
-						})}
-					</optgroup>
-				</>
-			);
-		});
+		return (
+			<>
+				{overall && (
+					<option key={'overall'} value={'0'}>
+						{overall}
+					</option>
+				)}{' '}
+				&&
+				{groupings.map((group) => {
+					return (
+						<optgroup key={group.category} label={group.category}>
+							{group.groups.map((grp) => (
+								<option key={grp.value} value={grp.value}>
+									{grp.label}
+								</option>
+							))}
+						</optgroup>
+					);
+				})}
+			</>
+		);
 	};
 
 	return (
-		<>
-			<FormControl sx={{ m: 1, minWidth: 120 }}>
-				<InputLabel htmlFor='grouped-native-select'>{props.label}</InputLabel>
-				<Select
-					native
-					defaultValue=''
-					id={`grouped-${props.label}-select`}
-					label={props.label}>
-					{optionGroups()}
-				</Select>
-			</FormControl>
-		</>
+		<FormControl sx={{ m: 1, minWidth: 120 }}>
+			<InputLabel htmlFor={`grouped-${label}-select`}>{label}</InputLabel>
+			<Select
+				native
+				value={value}
+				onChange={(e) => setValue(e.target.value as string)} // Cast to string
+				id={`grouped-${label}-select`}
+				label={label}>
+				{optionGroups()}
+			</Select>
+		</FormControl>
 	);
 }
