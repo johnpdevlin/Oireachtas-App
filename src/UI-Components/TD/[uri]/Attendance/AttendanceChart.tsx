29d6685cfee207c40f7aeebc26359425288900db
John Devlin <johnp.devlin2@gmail.com>
2024-04-05 21:23:07 +0100

    create attendance section and chart

---
+++ src/UI-Components/TD/[uri]/Attendance/AttendanceChart.tsx
@@ -0,0 +1,187 @@
+/** @format */
+
+import { Suspense, useEffect, useState } from 'react';
+import { AttendanceRecord } from '@/models/attendance';
+import { GroupType, URIpair } from '@/models/_utils';
+import { LineChart } from '@mui/x-charts';
+import { Breakpoint, Skeleton } from '@mui/material';
+
+type AttendanceChartProps = {
+	breakpoint: Breakpoint;
+	sidebarWidth?: number;
+	data: Record<GroupType, AttendanceRecord[]>;
+	chartType: GroupType;
+	keys: (URIpair & { color?: string })[];
+	year: string;
+};
+export default function AttendanceChart({
+	breakpoint,
+	sidebarWidth,
+	data,
+	chartType,
+	keys,
+	year,
+}: AttendanceChartProps) {
+	const [chartWidth, setChartWidth] = useState<number>(0);
+	const [chartHeight, setChartHeight] = useState<number>(0);
+
+	// Labels for x axis (i.e. years / months)
+	const [labels, setLabels] = useState<number[]>([]);
+	const [seriesData, setSeriesData] = useState<
+		{ label: string; data: number[]; color?: string }[]
+	>([]);
+
+	useEffect(() => {
+		let relativeWidth = 0;
+		let heightMutiplier = 0.35;
+
+		if (breakpoint === 'xl') relativeWidth = 1200 - sidebarWidth!;
+		else if (breakpoint === 'lg') relativeWidth = 1050 - sidebarWidth!;
+		else if (breakpoint === 'md') relativeWidth = 900 - sidebarWidth!;
+		else if (breakpoint === 'sm') relativeWidth = 625;
+		else if (breakpoint === 'xs') {
+			relativeWidth = 380;
+			heightMutiplier = 0.45;
+		}
+		setChartWidth(relativeWidth);
+		setChartHeight(relativeWidth * heightMutiplier);
+	}, [breakpoint]);
+
+	// If select changes, updates data states
+	useEffect(() => {
+		if (data) {
+			if (year === '0') {
+				setLabels(getYearLabels(data));
+				setSeriesData(getOverallSeriesData(data));
+			} else if (data && year !== '0') {
+				// console.info(getMonthLabels());
+				setSeriesData(getYearSeriesData(year, data));
+				setLabels(getMonthLabels(data));
+			}
+			console.info(seriesData);
+		}
+	}, [data, year]);
+
+	function getYearLabels(
+		data: Record<GroupType, AttendanceRecord[]>
+	): number[] {
+		return data[chartType]
+			.map((d) => d.year!)
+			.sort()
+			.map((d) => d);
+	}
+
+	function getMonthLabels(
+		data: Record<GroupType, AttendanceRecord[]>
+	): number[] {
+		const yearData = data[chartType].find((dat) => dat.year === parseInt(year));
+		if (!yearData) return []; // Return empty array if no data for selected year
+
+		const months = yearData.present_percentage?.months || [];
+		const relevantMonths: number[] = [];
+		months.forEach((value: number | undefined | null, index: number) => {
+			if (value !== undefined && value !== null) relevantMonths.push(index + 1);
+		});
+
+		return relevantMonths;
+	}
+	function getYearSeriesData(
+		year: string,
+		data: Record<GroupType, AttendanceRecord[]>
+	) {
+		const memberData = data[chartType].find(
+			(dat) => dat.year === parseInt(year)
+		);
+		if (!memberData) return []; // Return empty array if no data for selected year
+
+		const monthData = memberData.present_percentage?.months || [];
+		const relevantMonths: number[] = [];
+		monthData.forEach((value: number, index: number) => {
+			if (value !== undefined && value !== null) relevantMonths.push(index + 1);
+		});
+
+		return keys.map((key) => {
+			const groupData = data[key.uri as GroupType].find(
+				(dat) => dat.year === parseInt(year)
+			);
+			if (!groupData) return { label: key.name, data: [] }; // Return empty data array if no data for selected year
+
+			const groupMonthData = groupData.present_percentage?.months || [];
+			const relevantData: number[] = relevantMonths.map((month) => {
+				return groupMonthData[month - 1] !== undefined &&
+					groupMonthData[month - 1] !== null
+					? Number(groupMonthData[month - 1].toFixed(1))
+					: 0;
+			});
+
+			return {
+				label: key.name,
+				data: relevantData,
+				color: key.color ?? undefined,
+			};
+		});
+	}
+
+	// If Overall, resets data to aggregated years
+	function getOverallSeriesData(data: Record<GroupType, AttendanceRecord[]>) {
+		return Object.values(data).map((value: AttendanceRecord[]) => {
+			const length = data[chartType].length;
+			const key =
+				chartType === 'member'
+					? keys.find((key) => key.uri === value[0].group_type)
+					: keys.find((key) => key.uri === value[0].uri);
+
+			return {
+				label: key!.name,
+				data: value
+					.toSorted((a, b) => a.year! - b.year!)
+					.map((val) => Number(val.present_percentage?.overall.toFixed(1)))
+					.slice(value[0].group_type !== chartType ? length : 0),
+				color: key?.color,
+			};
+		});
+	}
+
+	return (
+		<>
+			<Suspense
+				fallback={
+					<Skeleton
+						variant='rectangular'
+						width={chartWidth}
+						height={chartHeight}
+					/>
+				}>
+				<LineChart
+					width={chartWidth}
+					height={chartHeight}
+					series={seriesData}
+					legend={{
+						hidden: true,
+					}}
+					margin={{ top: 25 }}
+					yAxis={[
+						{
+							label: '%',
+
+							max: 100,
+
+							maxTicks: 10,
+						},
+					]}
+					xAxis={[
+						{
+							label: year === '0' ? 'Years' : `Months (${year})`,
+							scaleType: year === '0' ? 'point' : undefined,
+							data: labels,
+							min: year != '0' ? 1 : labels.at(0),
+							max: year != '0' ? 12 : labels.at(-1),
+							minTicks: year != '0' ? 12 : labels.at(-1)! - labels.at(0)!,
+							maxTicks: year != '0' ? 12 : labels.at(-1)! - labels.at(0)!,
+						},
+					]}
+				/>
+			</Suspense>
+		</>
+	);
+}
---
+++ src/UI-Components/TD/[uri]/Attendance/AttendanceSection.tsx
@@ -0,0 +1,192 @@
+/** @format */
+import { Box, Grid, Stack, Typography } from '@mui/material';
+import AttendanceChart from '@/UI-Components/TD/[uri]/Attendance/AttendanceChart';
+import { MemberPageData } from '@/models/ui/member';
+import { useViewport } from '@/hooks/viewportProvider';
+import { getPartyColor } from '@/functions/_utils/parties';
+import { PartyCode } from '@/models/_utils';
+import ColoredBox from '@/UI-Components/_utils/ColoredBox';
+import GroupedSelect from '@/UI-Components/_utils/Select/GroupedSelect';
+import { useEffect, useState } from 'react';
+
+export default function AttendanceSection({ bio, attendance }: MemberPageData) {
+	const membershipKeys = [
+		{ uri: 'member', name: bio.fullName, color: '#FF1493' },
+		{
+			uri: 'party',
+			name: bio.parties[0].name,
+			color: getPartyColor(bio.parties[0].uri as PartyCode),
+		},
+		{
+			uri: 'constituency',
+			name: bio.constituencies.dail![0].name,
+			color:
+				bio.constituencies.dail![0].uri ===
+				('Independents_4_Change' || 'Workers_and_Unemployed_Action')
+					? '#4fc3f7'
+					: '#ffb74d',
+		},
+		{ uri: 'dail', name: 'Dáil', color: '#000000' },
+	];
+
+	const groupings = () => {
+		const groupingsObj = [];
+		const years = attendance.house.member.map((member) => {
+			return {
+				dateRange: member.dateRange,
+				year: member.year,
+			};
+		});
+
+		const dail33 = {
+			category: '33rd Dáil',
+			groups: years
+				.filter((year) => year.year! >= 2020 && year.year! > 2025)
+				.map((group) => {
+					return { label: group.year?.toString()!, value: group.year! };
+				})
+				.toSorted((a, b) => a.value - b.value),
+		};
+		const dail32 = {
+			category: '32nd Dáil',
+			groups: years
+				.filter((year) => year.year! < 20202)
+				.map((group) => {
+					{
+						return { label: group.year?.toString()!, value: group.year! };
+					}
+				})
+				.toSorted((a, b) => a.value - b.value),
+		};
+		dail33.groups.length > 0 && groupingsObj.push(dail33);
+		dail32.groups.length > 0 && groupingsObj.push(dail32);
+		return groupingsObj;
+	};
+
+	const [year, setYear] = useState<string>('0');
+	const { breakpoint } = useViewport();
+
+	const [sidebarWidth, setSidebarWidth] = useState(0);
+	const [sidebarDirection, setSidebarDirection] = useState('column');
+	const [legendDirection, setLegendDirection] = useState('column');
+
+	useEffect(() => {
+		const element = document.getElementById('chart-sidebar');
+		if (element) {
+			const width = element.offsetWidth;
+			setSidebarWidth(width);
+		}
+	}, []);
+
+	useEffect(() => {
+		if (breakpoint === 'xs' || breakpoint === 'sm') {
+			setSidebarDirection('row-reverse');
+			setLegendDirection('row');
+		} else if (
+			breakpoint === 'md' ||
+			breakpoint === 'lg' ||
+			breakpoint === 'xl'
+		) {
+			setSidebarDirection('column');
+			setLegendDirection('column');
+		}
+	}, [breakpoint]); // Empty dependency array ensures that this effect runs only once after initial render
+
+	const LegendComponent = () => {
+		return (
+			<Stack flexDirection={legendDirection} margin={2}>
+				{membershipKeys.map((key) => {
+					return (
+						<Stack
+							direction='row'
+							display='flex'
+							alignItems='center'
+							textOverflow='ellipsis '
+							key={key.uri}>
+							<ColoredBox color={key.color} size={'18px'} />
+							<Typography variant='body2'>{key.name}</Typography>
+						</Stack>
+					);
+				})}
+			</Stack>
+		);
+	};
+
+	return (
+		<>
+			<Grid container justifyContent='space-evenly' flexDirection='row-reverse'>
+				<Grid item display='flex' alignItems='start'>
+					<Stack flexDirection={sidebarDirection} gap={2} id={'chart-sidebar'}>
+						<GroupedSelect
+							label={'Period'}
+							groupings={groupings()}
+							value={year}
+							setValue={setYear}
+							overall={'Overall'}
+						/>
+						<Box
+							sx={{
+								display: {
+									xs: 'none',
+									sm: 'block',
+									md: 'block',
+									lg: 'block',
+									xl: 'block',
+								},
+							}}>
+							{LegendComponent()}
+						</Box>
+					</Stack>
+				</Grid>
+				<Grid item>
+					<Stack direction='column' gap={5}>
+						<Box>
+							<Stack direction='column'>
+								<Typography variant='h4' textAlign='center'>
+									House Attendance
+								</Typography>
+								<AttendanceChart
+									breakpoint={breakpoint}
+									sidebarWidth={sidebarWidth}
+									data={attendance.house}
+									chartType={'member'}
+									keys={membershipKeys}
+									year={year}
+								/>
+							</Stack>
+						</Box>
+						<Box>
+							<Stack direction='column'>
+								<Typography variant='h4' textAlign='center'>
+									Committee Attendance
+								</Typography>
+								<AttendanceChart
+									breakpoint={breakpoint}
+									sidebarWidth={sidebarWidth}
+									data={attendance.committee}
+									chartType={'member'}
+									keys={membershipKeys}
+									year={year}
+								/>
+							</Stack>
+						</Box>
+					</Stack>
+				</Grid>
+				<Box
+					sx={{
+						display: {
+							xs: 'block',
+							sm: 'none',
+							md: 'none',
+							lg: 'none',
+							xl: 'none',
+						},
+						alignItems: 'center',
+					}}
+					margin={1}>
+					{LegendComponent()}
+				</Box>
+			</Grid>
+		</>
+	);
+}
