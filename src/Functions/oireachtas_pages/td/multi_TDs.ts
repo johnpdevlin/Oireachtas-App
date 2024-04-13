a174d3dc3a3eadf24da90354f107224663251b67
John Devlin <johnp.devlin2@gmail.com>
2024-04-13 20:17:58 +0100

    add logic to add default return all current tds oir data

--- src/Functions/oireachtas_pages/td/multi_TDs.ts
+++ src/Functions/oireachtas_pages/td/multi_TDs.ts
@@ -1,12 +1,22 @@
 /** @format */
 
+import { MemberOirProfileData } from '@/models/member/oir_profile';
 import getMultiMembersOirDetails from './profile/multi_td_profiles';
-
-import { MemberOirProfile } from './profile/td_profile';
+import fetchMembers from '@/functions/APIs/Oireachtas/member/raw/_member_details';
+import houseDetails from '@/Data/house-details';
 
 export default async function getAllMembersOirData(
-	uris: string[]
-): Promise<MemberOirProfile[]> {
+	uris?: string[]
+): Promise<MemberOirProfileData[]> {
+	console.info('Scraping all data from Oireachtas profile for members...');
+	if (!uris)
+		uris = (
+			await fetchMembers({
+				chamber: 'dail',
+				house_no: houseDetails.dail.current,
+			})
+		)?.map((member) => member.memberCode) as string[];
+
 	const oirData = await getMultiMembersOirDetails(uris);
 
 	return oirData!;
