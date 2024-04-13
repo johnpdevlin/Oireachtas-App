575ffcb2e4f53615aa26015c556fb3a9dc800527
John Devlin <johnp.devlin2@gmail.com>
2024-04-13 20:14:32 +0100

    remove type to type file

--- src/Functions/oireachtas_pages/td/profile/td_profile.ts
+++ src/Functions/oireachtas_pages/td/profile/td_profile.ts
@@ -3,54 +3,52 @@ import axios from 'axios';
 import * as cheerio from 'cheerio';
 import he from 'he';
 import { extractWebsiteDomainName } from '@/functions/_utils/strings';
-
-export type MemberOirProfile = {
-	uri: string;
-	address: string;
-	contactNumbers: string[];
-	email: string;
-	webpages: WebsitePair[];
-};
+import { MemberOirProfileData } from '@/models/member/oir_profile';
 
 export type WebsitePair = { website: string | undefined; url: string };
 
 export default async function scrapeMemberOirProfile(
 	uri: string
-): Promise<MemberOirProfile> {
-	const url = `https://www.oireachtas.ie/en/members/member/${uri}`;
-	const response = he.decode(
-		(await axios.get(`api/webscrape?url=${url}`)).data.text
-	);
-	const $ = cheerio.load(response);
+): Promise<MemberOirProfileData | undefined> {
+	try {
+		const url = `https://www.oireachtas.ie/en/members/member/${uri}`;
+		const response = he.decode(
+			(await axios.get(`api/webscrape?url=${url}`)).data.text
+		);
+		const $ = cheerio.load(response);
 
-	const address: string = $(
-		'.c-member-about__address .c-member-about__item-value'
-	)
-		.text()
-		.trim();
+		const address: string = $(
+			'.c-member-about__address .c-member-about__item-value'
+		)
+			.text()
+			.trim();
 
-	const contactNumbers: string[] = $('.c-member-about__phone-numbers')
-		.text()
-		.trim()
-		.split('\n')
-		.filter((num: string) => num.trim().startsWith('('))
-		.map((num: string) => num.trim());
+		const contactNumbers: string[] = $('.c-member-about__phone-numbers')
+			.text()
+			.trim()
+			.split('\n')
+			.filter((num: string) => num.trim().startsWith('('))
+			.map((num: string) => num.trim());
 
-	const email: string = $('.c-member-about__email').text().trim();
+		const email: string = $('.c-member-about__email').text().trim();
 
-	const webpages: string[] = $('.c-member-about__web a')
-		.map((i, el) => {
-			return $(el).attr('href');
-		})
-		.get();
+		const webpages: string[] = $('.c-member-about__web a')
+			.map((i, el) => {
+				return $(el).attr('href');
+			})
+			.get();
 
-	return {
-		uri,
-		address,
-		contactNumbers,
-		email,
-		webpages: formatWebpages(webpages, uri),
-	};
+		return {
+			uri,
+			address,
+			contactNumbers,
+			email,
+			webpages: formatWebpages(webpages, uri),
+		};
+	} catch (e) {
+		console.error(e);
+		return undefined;
+	}
 }
 
 function formatWebpages(webpages: string[], uri: string): WebsitePair[] {
